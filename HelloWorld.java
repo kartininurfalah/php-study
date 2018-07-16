930a802e02e5512f0288f621432f99a7bcb4a09b
Kartini Nurfalah <kartininurfalah@gmail.com>
2018-07-16 10:29:21 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("Hello, World");
         System.out.println("Hello Falah");
         System.out.println("Kartini Nurfalah");
+        System.out.println("Telkom Indonesia");
     }
 
 }
